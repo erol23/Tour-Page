f8b2c74b1d0bc523af07a4226fe651e398b2b216
erol <hurkanrol15@gmail.com>
2021-05-02 23:12:08 +0300

    add card container

---
+++ src/components/cards/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Card.css';
+import { data } from '../../helpers/data';
+
+const Card = () => {
+    return(
+        <div className="card-container">
+            {data.map((card)=>{
+                <div className='cards'>
+                    <div className="title">
+                        <h2>{card.title}</h2>
+                    </div>
+                    <img src={card.image} alt={card.title}/>
+                    <div className="card-over">
+                        <p>{card.desc}</p>
+                    </div>
+                </div>
+            })}
+        </div>
+    )
+}
+
+export default Card;
